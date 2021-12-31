75c9837bce2bcd4957022496365085bbcd92e496
3timesv <vivek.verma662ha@gmail.com>
2021-12-31 15:08:32 +0530

    ps1 problem3 completed

--- ps1/src/twitter/SocialNetwork.java
+++ ps1/src/twitter/SocialNetwork.java
@@ -6,6 +6,10 @@ package twitter;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Arrays;
+import java.util.ArrayList;
 
 /**
  * SocialNetwork provides methods that operate on a social network.
@@ -41,7 +45,90 @@ public class SocialNetwork {
      *         either authors or @-mentions in the list of tweets.
      */
     public static Map<String, Set<String>> guessFollowsGraph(List<Tweet> tweets) {
-        throw new RuntimeException("not implemented");
+        // initialize result
+        Map<String, Set<String>> map = new HashMap<String, Set<String>>();
+
+        // iterate over tweets
+        for (Tweet tweet: tweets) {
+            // get the usernames mentioned in the tweet
+            Set<String> usernames = Extract.getMentionedUsers(Arrays.asList(tweet));
+
+            // if there are no usernames in the tweet, continue to next tweet
+            if (usernames.isEmpty()) {
+                continue;
+            }
+
+            Set<String> value;
+            // if the map contains the tweet's author as key
+            if (contains(map.keySet(), tweet.getAuthor())) {
+
+                // get the corresponding value
+                value = getValue(map, tweet.getAuthor());
+                // add the usernames mentioned in the tweet to the value
+                for (String user: usernames) {
+                    if (!contains(value, user)) {
+                        value.add(user);
+                    }
+                }
+                // replace the old value with new value
+                replaceValue(map, tweet.getAuthor(), value);
+                // otherwise
+            } else {
+                // create new set to map with the tweet's author
+                value = new HashSet<String>();
+                // add the usernames mentioned in the tweet to the value
+                for (String user: usernames) {
+                    if (!contains(value, user)) {
+                        value.add(user);
+                    }
+                }
+                // if the value is not empty, put the entry into the map
+                if (!value.isEmpty()) {
+                    map.put(tweet.getAuthor(), value);
+                }
+            }
+        }
+        return map;
+    }
+
+    // helper method to check whether a Set contains a target element (keeping in mind the case-insensitive criteria)
+    public static boolean contains(Set<String> collection, String target) {
+        for (String element: collection) {
+            if (element.toLowerCase().equals(target.toLowerCase())) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    // helper method to get the corresponding value associated with target key in the map (keeping in mind the case-insensitive criteria)
+    public static Set<String> getValue(Map<String, Set<String>> map, String targetKey) {
+        for (String key: map.keySet()) {
+            if (key.toLowerCase().equals(targetKey.toLowerCase())) {
+                return map.get(key);
+            }
+        }
+        return new HashSet<String>();
+    }
+
+    // helper method to replace the value associated with targetKey with new value in the map (keeping in mind the case_insensitive criteria) 
+    public static void replaceValue(Map<String, Set<String>> map, String targetKey, Set<String> value) {
+        for (String key: map.keySet()) {
+            if (key.toLowerCase().equals(targetKey.toLowerCase())) {
+                map.replace(targetKey, value);
+                break;
+            }
+        }
+    }
+
+    // helper function to increment the corresponding value of the key by 1 in the map (keeping in mind the case-insensitive criteria)
+    public static void increment(Map<String, Integer> map, String key) {
+        for (String k: map.keySet()) {
+            if (k.toLowerCase().equals(key.toLowerCase())) {
+                map.replace(k, map.get(k)+1);
+                break;
+            }
+        }
     }
 
     /**
@@ -54,7 +141,71 @@ public class SocialNetwork {
      *         descending order of follower count.
      */
     public static List<String> influencers(Map<String, Set<String>> followsGraph) {
-        throw new RuntimeException("not implemented");
-    }
+        // if the graph is empty, return empty List 
+        if (followsGraph.isEmpty()) {
+            return new ArrayList<String>();
+        }
 
+        // initialize an empty map to store the username and corresponding number of followers
+        Map<String, Integer> followers = new HashMap<>();
+
+        // fill the followers map 
+        for (String key: followsGraph.keySet()) {
+            // get all the usernames that follows key
+            Set<String> usernames = followsGraph.get(key);
+            // iterate over usernames 
+            for (String user: usernames) {
+                // if username is already in the followers map, increment its value by 1
+                if (contains(followers.keySet(), user)) {
+                    increment(followers, user);
+                    // otherwise, put the username in followers map with value 1
+                } else {
+                    followers.put(user, 1);
+                }
+            }
+        }
+
+        // initialize result ArrayList to store usernames by descending order of number of followers
+        List<String> result = new ArrayList<>();
+
+        // iterate over followers' keys
+        for (String key: followers.keySet()) {
+            // get the corresponding value
+            int value = followers.get(key);
+
+            // if result list is empty, add current key as the only element and continue to next element
+            if (result.isEmpty()) {
+                result.add(key);
+                continue;
+            } 
+
+            // get the first and last element of the result
+            String firstElement = result.get(0);
+            String lastElement = result.get(result.size()-1);
+
+            // if the element is larger than the first element, add it to the start of the result list
+            if (followers.get(firstElement) <= value) {
+                result.add(0, key);
+                continue;
+            }
+
+            // if the element is less than or equal to the last element, add it to the end of the result list
+            if (followers.get(lastElement) >= value) {
+                result.add(result.size(), key);
+                continue;
+            }
+
+            // otherwise, look at all the elements in result list and add the key at index i such that
+            //      the key at index i-1 has more followers than key at index i and 
+            //      the key at index i+1 has less followers than key at index i
+            for (int i=1; i < result.size()-1; i++) {
+                if (value <= followers.get(result.get(i-1)) && value >= followers.get(result.get(i+1))) {
+                    result.add(i, key);
+                    break;
+                }
+            }
+        }
+        // return the sorted list
+        return result;
+    }
 }
--- ps1/test/twitter/SocialNetworkTest.java
+++ ps1/test/twitter/SocialNetworkTest.java
@@ -7,39 +7,44 @@ import static org.junit.Assert.*;
 
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
+import java.time.Instant;
+import java.util.Arrays;
 
 import org.junit.Test;
 
 public class SocialNetworkTest {
 
-    /*
-     * TODO: your testing strategies for these methods should go here.
-     * See the ic03-testing exercise for examples of what a testing strategy comment looks like.
-     * Make sure you have partitions.
+    /*  Testing strategy
+     *
+     *  Partition for guessFollowsGraph(tweets) -> Map<String, Set<String>>
+     *
+     *  tweets.length = 0, 1, >1
+     *  Nobody follows nobody, everyone follows everyone else, >=1 users follow >=1 other users
+     *
+     *
+     *  Partition for influencers(Map<String, Set<String>> followsGraph) -> List<String>
+     *
+     *  followsGraph is empty or non-empty
+     *  resulting list is empty or non-empty
      */
-    
-    @Test(expected=AssertionError.class)
-    public void testAssertionsEnabled() {
-        assert false; // make sure assertions are enabled with VM argument: -ea
-    }
-    
-    @Test
-    public void testGuessFollowsGraphEmpty() {
-        Map<String, Set<String>> followsGraph = SocialNetwork.guessFollowsGraph(new ArrayList<>());
-        
-        assertTrue("expected empty graph", followsGraph.isEmpty());
-    }
-    
-    @Test
-    public void testInfluencersEmpty() {
-        Map<String, Set<String>> followsGraph = new HashMap<>();
-        List<String> influencers = SocialNetwork.influencers(followsGraph);
-        
-        assertTrue("expected empty list", influencers.isEmpty());
-    }
+
+    // covers followsGraph is empty and resulting list is empty
+
+    private static final Instant d1 = Instant.parse("2016-02-17T10:00:00Z");
+    private static final Instant d2 = Instant.parse("2016-02-17T11:00:00Z");
+
+    private static final Tweet tweet1 = new Tweet(1, "alyssa", "is it reasonable to talk about rivest so much?", d1);
+    private static final Tweet tweet2 = new Tweet(2, "bbitdiddle", "rivest talk in 30 minutes #hype", d2);
+    private static final Tweet tweet3 = new Tweet(3, "alex", "baby shark dudududu...", d1);
+    private static final Tweet tweet4 = new Tweet(4, "tim", "@cedric yes @fynn", d2);
+
+    private static final Tweet tweet5 = new Tweet(5, "cedric", "@tim yes @fynn", d2);
+    private static final Tweet tweet6 = new Tweet(6, "fynn", "@tim yes @cedric", d2);
+    private static final Tweet tweet7 = new Tweet(7, "Fynn", "@Tim is a good guy :)", d2);
 
     /*
      * Warning: all the tests you write here must be runnable against any
@@ -55,4 +60,168 @@ public class SocialNetworkTest {
      * class, then keep them in this test class.
      */
 
+    
+    @Test(expected=AssertionError.class)
+    public void testAssertionsEnabled() {
+        assert false; // make sure assertions are enabled with VM argument: -ea
+    }
+
+    // covers tweets.length = 0
+    @Test
+    public void testGuessFollowsGraphEmpty() {
+        Map<String, Set<String>> followsGraph = SocialNetwork.guessFollowsGraph(new ArrayList<>());
+        
+        assertTrue("expected empty graph", followsGraph.isEmpty());
+    }
+
+    // covers tweets.length = 1 and nobody follows nobody
+    @Test
+    public void testGuessFollowsGraphSingleTweetEmptyGraph() {
+        Map<String, Set<String>> followsGraph = SocialNetwork.guessFollowsGraph(new ArrayList<>());
+
+        assertTrue("expected empty graph", followsGraph.isEmpty());
+    }
+
+    // covers tweets.length = 1 and user follow >1 other users
+    @Test
+    public void testGuessFollowsGraphSingleTweetNonEmptyGraph() {
+        Map<String, Set<String>> followsGraph = SocialNetwork.guessFollowsGraph(Arrays.asList(tweet4));
+
+        Map<String, Set<String>> expectedResult = new HashMap<String, Set<String>>();
+        Set<String> followsSet = new HashSet<String>();
+        followsSet.add("cedric");
+        followsSet.add("fynn");
+        expectedResult.put("tim", followsSet);
+
+        assertTrue("expected non-empty graph", !followsGraph.isEmpty());
+        assertEquals("expected graphs to be equal", expectedResult, followsGraph);
+    }
+
+    // covers tweets.length > 1 and nobody follows nobody
+    @Test
+    public void testGuessFollowsGraphMultipleTweetsEmptyGraph() {
+        Map<String, Set<String>> followsGraph = SocialNetwork.guessFollowsGraph(Arrays.asList(tweet1, tweet2, tweet3));
+
+        assertTrue("expected empty graph", followsGraph.isEmpty());
+    }
+
+    // covers tweets.length > 1 and everyone follows everyone else
+    @Test
+    public void testGuessFollowsGraphMultipleTweetsFullGraph() {
+        Map<String, Set<String>> followsGraph = SocialNetwork.guessFollowsGraph(Arrays.asList(tweet4, tweet5, tweet6));
+
+        Map<String, Set<String>> expectedResult = new HashMap<String, Set<String>>();
+        Set<String> followsSet1 = new HashSet<String>();
+        followsSet1.add("cedric");
+        followsSet1.add("fynn");
+        expectedResult.put("tim", followsSet1);
+
+        Set<String> followsSet2 = new HashSet<String>();
+        followsSet2.add("tim");
+        followsSet2.add("fynn");
+        expectedResult.put("cedric", followsSet2);
+
+        Set<String> followsSet3 = new HashSet<String>();
+        followsSet3.add("tim");
+        followsSet3.add("cedric");
+        expectedResult.put("fynn", followsSet3);
+
+        assertTrue("expected non-empty grpah", !followsGraph.isEmpty());
+        assertEquals("expected graphs to be equal", expectedResult, followsGraph);
+    }
+
+    // covers tweets.length > 1 and >=1 users follow >=1 other users
+    @Test
+    public void testGuessFollowsGraphMultipleTweetsNonEmptyGraph() {
+        Map<String, Set<String>> followsGraph = SocialNetwork.guessFollowsGraph(Arrays.asList(tweet3, tweet5, tweet6, tweet7));
+
+        Map<String, Set<String>> expectedResult = new HashMap<String, Set<String>>();
+        Set<String> followsSet1 = new HashSet<String>();
+        followsSet1.add("tim");
+        followsSet1.add("fynn");
+        expectedResult.put("cedric", followsSet1);
+
+
+        assertFalse("expected non-empty graph", followsGraph.isEmpty());
+        assertTrue("expected key", followsGraph.containsKey("cedric"));
+        assertTrue("expected same values", followsGraph.get("cedric").equals(expectedResult.get("cedric")));
+
+        // the result can contain exactly one of the cases below:
+        //      - map[fynn] = {tim, cedric};
+        //      - map[fynn] = {Tim, cedric};
+        //      - map[Fynn] = {tim, cedric};
+        //      - map[Fynn] = {Tim, cedric};
+        assertEquals("expected same number of entries", 2, followsGraph.size());
+
+        Set<String> expectedFollows1 = new HashSet<String>();
+        expectedFollows1.add("tim");
+        expectedFollows1.add("cedric");
+
+        Set<String> expectedFollows2 = new HashSet<String>();
+        expectedFollows2.add("Tim");
+        expectedFollows2.add("cedric");
+
+        if (followsGraph.containsKey("fynn")) {
+            if (followsGraph.get("fynn").contains("tim")) {
+                assertEquals("expected same values", expectedFollows1, followsGraph.get("fynn"));
+            } else  {
+                assertEquals("expected same values", expectedFollows2, followsGraph.get("fynn"));
+            }
+        } else if (followsGraph.containsKey("Fynn")) {
+            if (followsGraph.get("Fynn").contains("tim")) {
+                assertEquals("expected same values", expectedFollows1, followsGraph.get("Fynn"));
+            } else  {
+                assertEquals("expected same values", expectedFollows2, followsGraph.get("Fynn"));
+            }
+        } else {
+            assertTrue("expected different key", false);
+        }
+    }
+
+    @Test
+    public void testInfluencersEmpty() {
+        Map<String, Set<String>> followsGraph = new HashMap<>();
+        List<String> influencers = SocialNetwork.influencers(followsGraph);
+        
+        assertTrue("expected empty list", influencers.isEmpty());
+    }
+
+    // covers followsGraph is non-empty and resulting list is also non-empty
+    @Test
+    public void testInfluencersNonEmpty() {
+        Map<String, Set<String>> followsGraph = new HashMap<>();
+        Set<String> timFollows = new HashSet<>();
+        timFollows.add("cedric");
+        timFollows.add("fynn");
+        followsGraph.put("tim", timFollows);
+
+        Set<String> fynnFollows = new HashSet<>();
+        fynnFollows.add("tim");
+        fynnFollows.add("Cedric");
+        followsGraph.put("fynn", fynnFollows);
+
+        List<String> result = SocialNetwork.influencers(followsGraph);
+
+        // result can be anyone of the following (all are valid)
+        List<String> expectedInfluencers1 = Arrays.asList("Cedric", "fynn", "tim");
+        List<String> expectedInfluencers2 = Arrays.asList("Cedric", "tim", "fynn");
+        List<String> expectedInfluencers3 = Arrays.asList("cedric", "fynn", "tim");
+        List<String> expectedInfluencers4 = Arrays.asList("cedric", "tim", "fynn");
+
+        if (result.get(0).equals("Cedric")) {
+            if (result.get(1).equals("fynn")) {
+                assertEquals("expected same list", expectedInfluencers1, result);
+            } else {
+                assertEquals("expected same list", expectedInfluencers2, result);
+            }
+        } else if (result.get(0).equals("cedric")) {
+            if (result.get(1).equals("fynn")) {
+                assertEquals("expected same list", expectedInfluencers3, result);
+            } else {
+                assertEquals("expected same list", expectedInfluencers4, result);
+            }
+        } else {
+            assertTrue("wrong result", false);
+        }
+    }
 }
